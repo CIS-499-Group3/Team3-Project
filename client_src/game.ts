b156be5e98d1a00d0a400ebd0f855453d4bf7861
Alan Boggess <alan.boggess@gmail.com>
2015-10-04 14:32:52 -0500

    Added some basic collision-testing parameters to the tilemap.  Will probably all eventually get thrown away.

--- client_src/game.ts
+++ client_src/game.ts
@@ -23,9 +23,29 @@ class PacmanGame {
     // Called by phaser to set up the game world.
     create(): void {
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        this.tilemap = this.game.add.tilemap('tiled2');
-        //this.tilemap.create('layer', 20, 20, 32 ,32);
-        //this.tilemap.putTile(1,4,4);
+        this.tilemap = this.game.add.tilemap('tiled2', 40, 40, 20,15);
+        this.tilemap.create('layer', 20, 15, 40, 40);
+		//eventually all of these for loops will be dispensed with once we have the game loading the maps from files, but these set up a map to test collision in properly until then.
+        //initializes borders of the map
+		for (var i=0; i < this.tilemap.width; i++) {
+			this.tilemap.putTile(0,i,0);
+		}
+		for (var i=0; i < this.tilemap.height; i++) {
+			this.tilemap.putTile(0,0,i);
+		}
+		for (var i=0; i < this.tilemap.width; i++) {
+			this.tilemap.putTile(0, i, this.tilemap.height-1);
+		}
+		for (var i=0; i < this.tilemap.height; i++) {
+			this.tilemap.putTile(0, this.tilemap.width-1, i);
+		}
+			
+		//add some collision tiles to the middle of the map
+		for (var i=3; i < 10; i++) {
+			this.tilemap.putTile(0,2,i);
+			this.tilemap.putTile(0,4,i);
+		}
+		this.tilemap.putTile(0,4,10)
 
         this.layer = this.tilemap.createLayer('layer')
         this.tilemap.addTilesetImage('testset');
@@ -38,9 +58,9 @@ class PacmanGame {
         //this.layer.resizeWorld()
 
 
-        //tilemap.fill(0,0,0,20,20);
-        //this.game.add.existing(tilemap);
-        this.player = new creature.PlayerPacman(this.game, 10, 10);
+        //this.tilemap.fill(0,0,0,20,20);
+        //this.game.add.existing(this.tilemap);
+        this.player = new creature.PlayerPacman(this.game, 2*this.tilemap.tileWidth, 2*this.tilemap.tileHeight);
     }
 
     // Called by phaser once per tick to update the game world.
