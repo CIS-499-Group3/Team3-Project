52f5600ee5d192e8b37f67f8d4852e350ea0abd8
DaveWeathers <beruay@gmail.com>
2015-10-05 23:22:57 -0500

    Game now has a dot that dies when you pass over it.
    Currently the dot is a global variable for the single dot, at some point
    we can make an array of dots or have a loop that spawns them based on
    tile.  Whatever seems best.

--- client_src/game.ts
+++ client_src/game.ts
@@ -1,12 +1,14 @@
 /// <reference path="../app/components/phaser/typescript/phaser.d.ts"/>
 import creature = require('./creature');
 import map = require('./map');
+import dot = require('./dot');
 
 class PacmanGame {
     private game: Phaser.Game;
 
     private player: creature.Pacman;
     private tilemap: Phaser.Tilemap;
+    private smallDot: dot.SmallDot;
     private layer: Phaser.TilemapLayer;
     constructor() {
         console.log("Yo!");
@@ -20,6 +22,7 @@ class PacmanGame {
         this.game.load.image('squarepacman', 'assets/squarepacman.png');
         this.game.load.image('testset', 'assets/testtileset.png');
         this.game.load.image('blinky', 'assets/blinky.png')
+        this.game.load.image('smalldot', 'assets/dot2.png');
         //this.game.load.tilemap('tiled2', 'assets/titled2.csv', null, Phaser.Tilemap.CSV);
         this.game.load.tilemap('tileset4', 'assets/tiled_4.csv', null, Phaser.Tilemap.CSV);
     }
@@ -49,16 +52,25 @@ class PacmanGame {
         this.player = new creature.PlayerPacman(this.game, pacMap, (10*this.tilemap.tileWidth)-40, (10*this.tilemap.tileHeight)-40);
 
         new creature.Ghost(this.game, pacMap, 50, 50);
+        
+        this.smallDot = new dot.SmallDot(this.game, pacMap, (10*this.tilemap.tileWidth)-160, (10*this.tilemap.tileHeight)-100);
     }
 
     // Called by phaser once per tick to update the game world.
     update(): void {
+    
+            this.game.physics.arcade.overlap(this.player, this.smallDot, collectDot, null, this);
         this.game.physics.arcade.collide(this.player, this.layer);
         this.game.physics.arcade.collide(this.player, this.layer, (s,t) => {
             console.log("Collide " + s + " " + t)
+            //dot collision
+            
         });
     }
 
 }
+function collectDot(Creature, Dot){
+this.smallDot.destroy();
 
+}
 var game: PacmanGame = new PacmanGame();
