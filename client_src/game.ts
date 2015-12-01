81c31cd341f9ce732eef0a40e83b78d104258837
Alan Boggess <alan.boggess@gmail.com>
2015-12-01 00:32:07 -0600

    Implemented map transitions on "win."
    I've implemented this by "destroying" the whole playing field when the last dot has been consumed and re-drawing the whole board, so this may introduce a whole bunch of bugs I can't foresee.  The good news is that if the bugs it introduces can be fixed without a huge amount of work, this ought to be scalable to as many maps as we want.

--- client_src/game.ts
+++ client_src/game.ts
@@ -4,7 +4,10 @@ import map = require('./map');
 import dot = require('./dot');
 import util = require('./util');
 
+
+
 class PacmanGame {
+    static mapsAssets: string[] = ['assets/original_pacman_map.csv', 'assets/original_pacman_map2.csv'];
     private game: Phaser.Game;
 
     private player: creature.Pacman;
@@ -18,6 +21,7 @@ class PacmanGame {
 
     public score: number;
     public lives: number;
+    private mapCount: number;
     private scoreText;
     private livesText;
 
@@ -26,19 +30,20 @@ class PacmanGame {
         this.game = new Phaser.Game(800, 800, Phaser.AUTO, 'game-div', this);
         this.score = 0;
         this.lives = 3;
+        //this.lives = 99;
+        this.mapCount = 0;
         this.smallDotMap = [];
     }
     
     // Called by phaser to preload resources.
     preload(): void {
-        
         this.game.load.image('badpacman', 'assets/awesomePacman.png');
         this.game.load.atlasJSONHash('pacman', 'assets/pacmove.png', 'assets/pacmove.json')
         this.game.load.image('testset', 'assets/testtileset.png');
         this.game.load.atlasJSONHash('blinky', 'assets/blinkymove.png', 'assets/blinkymove.json')
         //this.game.load.image('blinky', 'assets/blinky.png')
         this.game.load.image('smalldot', 'assets/dot2.png');
-        this.game.load.tilemap('tileset', 'assets/original_pacman_map.csv', null, Phaser.Tilemap.CSV);
+        this.game.load.tilemap('map0', PacmanGame.mapsAssets[0], null, Phaser.Tilemap.CSV);
         this.game.load.image('pacmanchunk', 'assets/pacman_chunk.png');
     }
 
@@ -47,76 +52,15 @@ class PacmanGame {
     // Called by phaser to set up the game world.
     create(): void {
         console.log(this);
-        this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        let tilemap = this.game.add.tilemap('tileset');
-        //this.tilemap.create('layer', 20, 20, 32 ,32);
-        //this.tilemap.putTile(1,4,4);
-
-        this.layer = tilemap.createLayer('layer');
-        tilemap.addTilesetImage('testset');
-
-
-        tilemap.setCollision(1, true, this.layer);
-        tilemap.setCollision([0,2,3,5], false, this.layer); // Set floors to not collide.
-
-        // Oddly enough, the 'score' value in the constructor doesn't hold and I don't know why.
-        // Try it out:
-        //console.log('Score is ' + this.score);
-        this.score = 0;
-
-        var pacMap: map.PacMap = new map.PacMap(tilemap);
-        this.map = pacMap;
-
-        // Find where pacman should spawn.
-        let pacmanSpawnTile = util.randomChoice(pacMap.getPacmanSpawns());
-        this.player = new creature.PlayerPacman(this.game, pacMap, pacmanSpawnTile.getX(), pacmanSpawnTile.getY());
-        
-        // Blinky, for the deliverables.
-	    let blinkySpawnTile = pacMap.getBlinkySpawns() [0];
-	    let inkySpawnTile = pacMap.getInkySpawns() [0];
-        this.blinky1 = new creature.CornersGhost(this.game, pacMap, blinkySpawnTile.getX(), blinkySpawnTile.getY(), "blinky");
-        this.blinky2 = new creature.ScanningGhost(this.game, pacMap, inkySpawnTile.getX(), inkySpawnTile.getY(), "blinky");
-        this.blinky1.body.immovable = true;
-        this.blinky2.body.immovable = true;
-
-        //this.smallDot = new dot.SmallDot(this.game, pacMap, (10*this.tilemap.tileWidth)-160, (10*this.tilemap.tileHeight)-100);
-        //console.log(this.smallDotMap);
-
-        //Initialize the list of dots from the map data.
-        this.smallDotMap = [];
-        var dotTiles = pacMap.allTilesWithID(map.TileID.FLOOR);
-        for (var d of dotTiles){
-            var x = d.getCenterX();
-            var y = d.getCenterY();
-            this.smallDotMap.push(new dot.SmallDot(this.game, pacMap, x, y));
-        }
-
-        //Initialize the list of Teleport Tiles
-        this.teleportTiles = [];
-        let tTiles = pacMap.allTilesWithID(map.TileID.TELEPORT);
-        for (var tile of tTiles){
-            this.teleportTiles.push(tile.getTile());
-        }
-
-        //Set the special rules for teleport tiles.
-        pacMap.getTilemap().setTileIndexCallback(map.TileID.TELEPORT, (creature, tile) => {
-            creature.explode('pacmanchunk');
-            let destination = this.teleportTiles[(this.teleportTiles.indexOf(tile)+1)%this.teleportTiles.length];
-            this.player.x = this.player.getMap().viewOf(destination.x, destination.y).getCenterX();
-            this.player.y = this.player.getMap().viewOf(destination.x, destination.y).getCenterY();
-            if (this.player.getContainingTile().getTile().x == 0)
-                this.player.x = this.player.getContainingTile().viewEast().getCenterX();
-            if (this.player.getContainingTile().getTile().x == tilemap.width-1)
-                this.player.x = this.player.getContainingTile().viewWest().getCenterX();
-            if (this.player.getContainingTile().getTile().y == 0)
-                this.player.x = this.player.getContainingTile().viewSouth().getCenterY();
-            if (this.player.getContainingTile().getTile().y == tilemap.height-1)
-                this.player.x = this.player.getContainingTile().viewWest().getCenterY();
-        }, this);
+        this.initializeMap('map0');
 
         //The score
-        this.scoreText = this.game.add.text((20*tilemap.tileWidth), (tilemap.tileHeight), 'Score:0', { fontSize: '32px', fill: '#0000FF' });
-        this.livesText = this.game.add.text((20*tilemap.tileWidth), (tilemap.tileHeight + 40), 'AAA', { fontSize: '32px', fill: '#0000FF' });
+        this.scoreText = this.game.add.text((20*this.map.getTilemap().tileWidth),
+                                            (this.map.getTilemap().tileHeight),
+                                            'Score:0', { fontSize: '32px', fill: '#0000FF' });
+        this.livesText = this.game.add.text((20*this.map.getTilemap().tileWidth),
+                                            (this.map.getTilemap().tileHeight + 40),
+                                            'AAA', { fontSize: '32px', fill: '#0000FF' });
         this.updateLivesText();
     }
     
@@ -169,7 +113,19 @@ class PacmanGame {
 
     // Called when the game has been won.
     private onWin(): void {
-        alert("Game won!");
+        this.game.physics.arcade.isPaused = true;
+        this.game.time.events.add(1000, () => {
+            this.player.destroy();
+            this.blinky1.destroy();
+            this.blinky2.destroy();
+            this.map.getTilemap().destroy();
+            this.mapCount++;
+            this.game.load.tilemap('map' + this.mapCount,
+                                PacmanGame.mapsAssets[this.mapCount%PacmanGame.mapsAssets.length],
+                                null, Phaser.Tilemap.CSV);
+            this.game.load.start();
+            this.game.load.onLoadComplete.add(game => {this.initializeMap('map' + this.mapCount)});
+        });
     }
 
     private onLose(): void {
@@ -211,6 +167,75 @@ class PacmanGame {
             this.respawnPlayer();
         }
 
+    }
+
+    private initializeMap(name: string){
+        let tilemap = this.game.add.tilemap(name);
+        //this.tilemap.create('layer', 20, 20, 32 ,32);
+        //this.tilemap.putTile(1,4,4);
+
+        this.layer = tilemap.createLayer('layer');
+        tilemap.addTilesetImage('testset');
+
+
+        tilemap.setCollision(1, true, this.layer);
+        tilemap.setCollision([0,2,3,5], false, this.layer); // Set floors to not collide.
+
+        // Oddly enough, the 'score' value in the constructor doesn't hold and I don't know why.
+        // Try it out:
+        //console.log('Score is ' + this.score);
+        this.score = 0;
+
+        var pacMap: map.PacMap = new map.PacMap(tilemap);
+        this.map = pacMap;
+
+        // Find where pacman should spawn.
+        let pacmanSpawnTile = util.randomChoice(pacMap.getPacmanSpawns());
+        this.player = new creature.PlayerPacman(this.game, this.map, pacmanSpawnTile.getX(), pacmanSpawnTile.getY());
+
+        // Blinky, for the deliverables.
+        let blinkySpawnTile = pacMap.getBlinkySpawns() [0];
+        let inkySpawnTile = pacMap.getInkySpawns() [0];
+        this.blinky1 = new creature.CornersGhost(this.game, pacMap, blinkySpawnTile.getX(), blinkySpawnTile.getY(), "blinky");
+        this.blinky2 = new creature.ScanningGhost(this.game, pacMap, inkySpawnTile.getX(), inkySpawnTile.getY(), "blinky");
+        this.blinky1.body.immovable = true;
+        this.blinky2.body.immovable = true;
+
+        //this.smallDot = new dot.SmallDot(this.game, pacMap, (10*this.tilemap.tileWidth)-160, (10*this.tilemap.tileHeight)-100);
+        //console.log(this.smallDotMap);
+
+        //Initialize the list of dots from the map data.
+        this.smallDotMap = [];
+        var dotTiles = pacMap.allTilesWithID(map.TileID.FLOOR);
+        for (var d of dotTiles){
+            var x = d.getCenterX();
+            var y = d.getCenterY();
+            this.smallDotMap.push(new dot.SmallDot(this.game, pacMap, x, y));
+        }
+
+        //Initialize the list of Teleport Tiles
+        this.teleportTiles = [];
+        let tTiles = pacMap.allTilesWithID(map.TileID.TELEPORT);
+        for (var tile of tTiles){
+            this.teleportTiles.push(tile.getTile());
+        }
+
+        //Set the special rules for teleport tiles.
+        pacMap.getTilemap().setTileIndexCallback(map.TileID.TELEPORT, (creature, tile) => {
+            creature.explode('pacmanchunk');
+            let destination = this.teleportTiles[(this.teleportTiles.indexOf(tile)+1)%this.teleportTiles.length];
+            this.player.x = this.player.getMap().viewOf(destination.x, destination.y).getCenterX();
+            this.player.y = this.player.getMap().viewOf(destination.x, destination.y).getCenterY();
+            if (this.player.getContainingTile().getTile().x == 0)
+                this.player.x = this.player.getContainingTile().viewEast().getCenterX();
+            if (this.player.getContainingTile().getTile().x == tilemap.width-1)
+                this.player.x = this.player.getContainingTile().viewWest().getCenterX();
+            if (this.player.getContainingTile().getTile().y == 0)
+                this.player.x = this.player.getContainingTile().viewSouth().getCenterY();
+            if (this.player.getContainingTile().getTile().y == tilemap.height-1)
+                this.player.x = this.player.getContainingTile().viewWest().getCenterY();
+        }, this);
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
     }
 }
 
